cc98e38a34384899f90d7e8d29e186cb996f883f
chrissunny94 <chrissunny94@gmail.com>
2024-05-04 06:40:12 +0530

    back_bone.py

---
+++ main/back_bone.py
@@ -0,0 +1,53 @@
+from __future__ import division
+import dlib
+from imutils import face_utils
+import cv2
+import numpy as np
+from scipy.spatial import distance as dist
+import threading
+import pygame
+ 
+# initialize dlib's face detector (HOG-based) and then create
+# the facial landmark predictor
+path = "artifacts/shape_predictor_68_face_landmarks.dat"
+detector = dlib.get_frontal_face_detector()
+predictor = dlib.shape_predictor(path)
+
+cap = cv2.VideoCapture(1)
+ 
+while True:
+    # load the input image and convert it to grayscale
+    ret, image = cap.read()
+    if ret == False:
+        print('Failed to capture frame from camera. Check camera index in cv2.VideoCapture(0) \n')
+        break
+    gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+        
+    # detect faces in the grayscale image
+    rects = detector(gray, 0)
+    height, width = gray.shape
+    print(height, width)
+    mask = np.zeros((height, width, 2), np.uint8)
+    
+    
+    # loop over the face detections
+    for (i, rect) in enumerate(rects):
+        # determine the facial landmarks for the face region, then
+        # convert the facial landmark (x, y)-coordinates to a NumPy
+        # array
+        shape = predictor(gray, rect)
+        shape = face_utils.shape_to_np(shape)
+    
+        # loop over the (x, y)-coordinates for the facial landmarks
+        # and draw them on the image
+        for (x, y) in shape:
+            cv2.circle(mask, (x, y), 2, (0, 255, 0), -1)
+    
+    # show the output image with the face detections + facial landmarks
+    cv2.imshow("Output", mask)
+    k = cv2.waitKey(5) & 0xFF
+    if k == 27:
+        break
+
+cv2.destroyAllWindows()
+cap.release()
